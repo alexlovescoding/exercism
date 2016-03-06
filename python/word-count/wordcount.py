dcce04be7613c696395cd6e03775e815263f9f0b
Alex Johnson <me@alex-johnson.net>
2016-03-06 01:10:54 -0500

    removed print

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -5,5 +5,4 @@ def word_count(phrase):
     phrase = phrase.decode("utf-8").lower()
     r = re.compile(r'[\s{}]+'.format(re.escape(punctuation)))
     phraseList = r.split(phrase)
-    print(phraseList)
     return {word: phraseList.count(word) for word in set(phraseList) if word != ''}
