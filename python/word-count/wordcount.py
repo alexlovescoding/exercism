b298e92d85b9e95bffcdddae732891b4929b22b2
Alex Johnson <me@alex-johnson.net>
2016-03-06 13:34:52 -0500

    got wordcount working

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -3,6 +3,6 @@ from string import punctuation
 
 def word_count(phrase):
     phrase = phrase.decode("utf-8").lower()
-    r = re.compile(r'[\s{}]+'.format(re.escape(punctuation)))
+    r = re.compile(ur'[\s{}\U0001f596]+'.format(re.escape(punctuation)))
     phraseList = r.split(phrase)
     return {word: phraseList.count(word) for word in set(phraseList) if word != ''}
