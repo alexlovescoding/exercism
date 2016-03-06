1b4a9e1d714ee8a45287e0f24e8d5e781bf7bc9d
Alex Johnson <me@alex-johnson.net>
2016-03-05 23:13:27 -0500

    solved Java Etl problem

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,8 +1,17 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Etl {
    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
+      Set<Integer> keys = old.keySet();
+      Map<String, Integer> result = new HashMap<String, Integer>();
+      for(Integer key: keys) {
+         for(String str: old.get(key)) {
+           result.put(str.toLowerCase(), key);
+         }
+      }
+      return result;
    }
 }
