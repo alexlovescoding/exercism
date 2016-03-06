adc70362165029fe4dea94f294ed2f51e4166193
Alex Johnson <me@alex-johnson.net>
2016-03-06 16:35:10 -0500

    fixed some nits from exercism

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,7 +1,3 @@
-// Clock stub file
-
-// To use the right term, this is the package *clause*.
-// You can document general stuff about the package here if you like.
 package clock
 
 import "fmt"
@@ -10,29 +6,29 @@ import "fmt"
 // clock_test.go.
 
 const (
-  testVersion = 3
-  minutesPerDay = 1440
-  minutesPerHour = 60
+	testVersion    = 3
+	minutesPerDay  = 1440
+	minutesPerHour = 60
 )
 
 type Clock int
 
 func New(hour, minutes int) Clock {
-  var m int = (hour * minutesPerHour + minutes) % minutesPerDay
-  if m < 0 {
-    m += minutesPerDay
-  }
-  return Clock(m)
+	var m int = (hour*minutesPerHour + minutes) % minutesPerDay
+	if m < 0 {
+		m += minutesPerDay
+	}
+	return Clock(m)
 }
 
 func (m Clock) String() string {
-  return fmt.Sprintf("%02d:%02d", m/minutesPerHour, m%minutesPerHour)
+	return fmt.Sprintf("%02d:%02d", m/minutesPerHour, m%minutesPerHour)
 }
 
 func (m Clock) Add(minutes int) Clock {
-  m = (m + Clock(minutes)) % minutesPerDay
-  if m < 0 {
-    m += minutesPerDay
-  }
-  return m
+	m = (m + Clock(minutes)) % minutesPerDay
+	if m < 0 {
+		m += minutesPerDay
+	}
+	return m
 }
