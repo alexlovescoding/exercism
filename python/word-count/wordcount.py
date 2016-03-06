155439c2e9ea0074bdfef14caf76a3c3ca28cbeb
Alex Johnson <me@alex-johnson.net>
2016-03-06 01:05:36 -0500

    almost have the wordcount exercise working

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,7 +1,9 @@
+import re
+from string import punctuation
+
 def word_count(phrase):
-    phraseList = phrase.split()
-    phraseSet = set(phraseList)
-    dict = {}
-    for word in phraseSet:
-        dict[word] = phraseList.count(word)
-    return dict
+    phrase = phrase.decode("utf-8").lower()
+    r = re.compile(r'[\s{}]+'.format(re.escape(punctuation)))
+    phraseList = r.split(phrase)
+    print(phraseList)
+    return {word: phraseList.count(word) for word in set(phraseList) if word != ''}
