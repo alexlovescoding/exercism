ca42884925ba5cb688c8fc2c77ce8c743f909667
Alex Johnson <me@alex-johnson.net>
2016-03-13 08:50:45 -0400

    do gigasecond without importing math

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,15 +1,12 @@
 // Package gigasecond has the general stuff for the package.
 package gigasecond
 
-import (
-	"math"
-	"time"
-)
+import "time"
 
 // testVersion here must match `targetTestVersion` in the file gigasecond_test.go.
 const testVersion = 4
 
 // AddGigasecond adds a gigasecond (10**9 seconds) to the given time.
-func AddGigasecond(m time.Time) time.Time {
-	return m.Add(time.Duration(math.Pow(10, 9) * math.Pow(10, 9)))
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(time.Duration(1000000000000000000))
 }
