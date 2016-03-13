baf7d4553073d66c20b7b1652847c37c40835212
Alex Johnson <me@alex-johnson.net>
2016-03-13 15:35:26 -0400

    completed go raindrops exercise

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,7 +1,27 @@
 package raindrops
 
+import (
+	"bytes"
+	"strconv"
+)
+
 const testVersion = 2
 
-func Convert(int) string
+func Convert(num int) string {
+	if num%3 != 0 && num%5 != 0 && num%7 != 0 {
+		return strconv.Itoa(num)
+	}
 
-// The test program has a benchmark too.  How fast does your Convert convert?
+	var sb bytes.Buffer
+
+	if num%3 == 0 {
+		sb.WriteString("Pling")
+	}
+	if num%5 == 0 {
+		sb.WriteString("Plang")
+	}
+	if num%7 == 0 {
+		sb.WriteString("Plong")
+	}
+	return sb.String()
+}
