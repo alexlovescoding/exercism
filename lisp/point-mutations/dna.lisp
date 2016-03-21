7a2208bae08bebf766b0945a8f43229926a68841
Alex Johnson <me@alex-johnson.net>
2016-03-21 03:08:43 -0400

    got something down for the first lisp exercise, but doesn't completely work

--- lisp/point-mutations/dna.lisp
+++ lisp/point-mutations/dna.lisp
@@ -3,6 +3,15 @@
   (:export #:hamming-distance))
 (in-package #:dna)
 
-(defun hamming-distance (dna1 dna2)
 
-)
+(defun hamming-distance (dna1 dna2)
+(cond ((string= dna1 dna2)
+    0)
+  ((/= (length dna1) (length dna2))
+    nil)
+  (t (setq count 0)
+    (loop for i from 0 to (length dna1)
+        do (if (not (eq (char dna1 i) (char dna2 i)))
+          (setq count (+ 1 count))))
+    count)
+))
