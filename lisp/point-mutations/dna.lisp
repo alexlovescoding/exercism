a2ea72b03c20d77cc8738416848ae3eaed79e110
Alex Johnson <me@alex-johnson.net>
2016-03-23 12:36:52 -0400

    fixed lisp exercise, still need to fix dem nits

--- lisp/point-mutations/dna.lisp
+++ lisp/point-mutations/dna.lisp
@@ -10,7 +10,7 @@
   ((/= (length dna1) (length dna2))
     nil)
   (t (setq count 0)
-    (loop for i from 0 to (length dna1)
+    (loop for i from 0 to (- (length dna1) 1)
         do (if (not (eq (char dna1 i) (char dna2 i)))
           (setq count (+ 1 count))))
     count)
