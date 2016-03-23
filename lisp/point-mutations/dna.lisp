4ebcc164e5f4d4b5bd4e9df5ff623bf4eadf6e44
Alex Johnson <me@alex-johnson.net>
2016-03-23 13:58:04 -0400

    made formatting better

--- lisp/point-mutations/dna.lisp
+++ lisp/point-mutations/dna.lisp
@@ -5,13 +5,9 @@
 
 
 (defun hamming-distance (dna1 dna2)
-(cond ((string= dna1 dna2)
-    0)
-  ((/= (length dna1) (length dna2))
-    nil)
-  (t (setq count 0)
-    (loop for i from 0 to (- (length dna1) 1)
+  (cond ((string= dna1 dna2) 0)
+    ((/= (length dna1) (length dna2)) nil)
+    (t (setq count 0)
+      (loop for i from 0 to (- (length dna1) 1)
         do (if (not (eq (char dna1 i) (char dna2 i)))
-          (setq count (+ 1 count))))
-    count)
-))
+          (setq count (+ 1 count)))) count)))
