70cb9436f92baa8b4f73e3f685e962ed875af1c4
Alex Johnson <me@alex-johnson.net>
2016-04-28 00:40:21 -0400

    completed hamming exercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,7 +1,22 @@
 package hamming
 
+import "errors"
+
 const testVersion = 4
 
-func Distance(a, b string) (int, error) {
-	// your code here!
+func Distance(s1, s2 string) (count int, err error) {
+	r1 := []rune(s1)
+	r2 := []rune(s2)
+
+	if len(r1) != len(r2) {
+		err = errors.New("The strings need to be the same length")
+		return
+	}
+
+	for i, c := range r1 {
+		if r2[i] != c {
+			count++
+		}
+	}
+	return
 }
