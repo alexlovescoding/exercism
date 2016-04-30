28e1242d59d73ecd2a9987f4cfd01128512ec41a
Alex Johnson <me@alex-johnson.net>
2016-04-30 18:24:43 -0400

    traingle exercise in Go

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,18 +1,35 @@
 package triangle
 
+import (
+	"math"
+	"sort"
+)
+
 const testVersion = 2
 
-// Kind tells you whether a triangle with the given sides exists if so,
+// KindFromSides tells you whether a triangle with the given sides exists if so,
 // it tells you whether the triangle is equilateral, isosceles, or scalene.
 func KindFromSides(a, b, c float64) Kind {
-
+  sides := []float64{a,b,c}
+  sort.Sort(sort.Float64Slice(sides))
+  switch {
+    case math.IsNaN(sides[0]) || sides[0] <= 0 || sides[2] >= math.Inf(1) ||
+          sides[0]+sides[1] < sides[2]:
+      return NaT
+    case sides[0] == sides[2]:
+      return Equ
+    case sides[0] == sides[1] || sides[1] == sides[2]:
+      return Iso
+    default:
+      return Sca
+  }
 }
 
-// Notice it returns this type.  Pick something suitable.
-type Kind
-
-// Pick values for the following identifiers used by the test program.
-NaT // not a triangle
-Equ // equilateral
-Iso // isosceles
-Sca // scalene
+// Kind: The type that gets returned
+type Kind string
+const (
+  NaT = "NaT" // not a triangle
+  Equ = "Equ" // equilateral
+  Iso = "Iso" // isosceles
+  Sca = "Sca" // scalene
+)
