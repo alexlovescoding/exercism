51524581d62971ef2030880487214081b3a01e75
Alex Johnson <hello@alex-johnson.net>
2019-09-13 19:42:29 -0700

    Init repo
    Signed-off-by: Alex Johnson <hello@alex-johnson.net>

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,12 @@
+// Package gigasecond has the general stuff for the package.
+package gigasecond
+
+import "time"
+
+// testVersion here must match `targetTestVersion` in the file gigasecond_test.go.
+const testVersion = 4
+
+// AddGigasecond adds a gigasecond (10**9 seconds) to the given time.
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(1e18)
+}
