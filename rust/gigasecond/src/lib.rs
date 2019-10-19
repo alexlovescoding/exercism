59721a80c68f50a1ebf71f1208941e92316471d4
Alex Johnson <hello@alex-johnson.net>
2019-10-19 15:42:03 -0700

    Update gigasecond exercism in rust track
    Signed-off-by: Alex Johnson <hello@alex-johnson.net>

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,5 @@
 use chrono::{DateTime, Duration, Utc};
-use std::ops::Add;
 
 pub fn after(time: DateTime<Utc>) -> DateTime<Utc> {
-    let gigasecond = (10 as i64).pow(9);
-    time.add(Duration::seconds(gigasecond))
+    return time + Duration::seconds(1_000_000_000);
 }
