e57f52f192a1e3277eb36eaeada5ec70b0b66e9e
Alex Johnson <hello@alex-johnson.net>
2019-11-05 10:02:48 -0800

    Complete the word-count exercise in elixir track
    Signed-off-by: Alex Johnson <hello@alex-johnson.net>

---
+++ elixir/word-count/lib/words.ex
@@ -0,0 +1,28 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    count(String.split(String.downcase(sentence), ~r/[!"#$%&'()*+,.\/:;<=>?@[\]^_`{|}~\s]/), %{})
+  end
+
+  @spec count([String.t()], map) :: map
+  defp count([head | tail], countMap) do
+    cond do
+      head == "" ->
+        count(tail, countMap)
+      Map.has_key?(countMap, head) ->
+        count(tail, %{countMap | head => countMap[head] + 1})
+      true ->
+        count(tail, Map.put(countMap, head, 1))
+    end
+  end
+
+  @spec count([], map) :: map
+  defp count([], countMap) do
+    countMap
+  end
+end
